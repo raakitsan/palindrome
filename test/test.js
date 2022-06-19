0e45e7e996bc38e7673ac34df4fa19d450e8f8a2
Brian Raak <bjraak@uw.edu>
2022-06-19 18:35:37 -0500

    Finish working and refactored palindrome method

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ let Phrase = require("../index.js");
 
 describe("Phrase", function() {
 
-describe("#palindrome", function() {
+  describe("#palindrome", function() {
 
     it("should return false for a non-palindrome", function() {
       let nonPalindrome = new Phrase("apple");
@@ -15,8 +15,21 @@ describe("#palindrome", function() {
       assert(plainPalindrome.palindrome());
     });
 
-    it("should return true for a mixed-case palindrome"); 
+    it("should return true for a mixed-case palindrome", function() {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+    });
 
-    it("should return true for a palindrome with punctuation");
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
   });
-});
+
+  describe("#letters", function() {
+    it("should return only letters", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+    });
+  });
+})
